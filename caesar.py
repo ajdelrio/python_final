2216b093e269d511bfa1a50f94cff6a6b00bc9df
Alex del Rio <alex.j.delrio@gmail.com>
2019-05-04 17:00:53 -0700

    fixed caesar.py

--- caesar.py
+++ caesar.py
@@ -81,16 +81,4 @@ def decrypt():
             break
         if inp == "n" or "N":
             break
-
-
-while True:
-    choice = input("Would you like to Encrypt(1) or Decrypt(2) text?\n"
-                   "(Select 1 or 2 or q to Quit)\n")
-    if choice == "q" or choice == "Q":
-        break
-    if choice == "1":
-        encrypt()
-    elif choice == "2":
-        decrypt()
-    else:
-        print("\nInvalid choice.\n")
+            
