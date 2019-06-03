48c1edebb1d571c84f93ba91869ea1a355cf86c1
Alex del Rio <alex.j.delrio@gmail.com>
2019-06-02 21:47:12 -0700

    Added comments, fixed some spelling errors in strings

--- caesar.py
+++ caesar.py
@@ -1,4 +1,5 @@
 def getShift():
+    """Gets the shift value from the user and validates it"""
     while True:
         try:
             shift = int(input(
@@ -14,6 +15,8 @@ def getShift():
 
 
 def shiftText(text, shift):
+    """Shifts each word in the provided text and returns the result
+    """
     old_text = text.upper()
     lyst = old_text.split()
     new_list = []
@@ -25,13 +28,16 @@ def shiftText(text, shift):
 
 
 def shiftWord(word, shift):
+    """Shifts each alphabetical character the desired value, wraps text from z
+    to a as well
+    """
     new_word = ""
     for i in range(len(word)):
         if word[i].isalpha() is False:
             continue
         if ord(word[i]) + shift in range(65, 91):
             new_word += chr(ord(word[i]) + shift)
-        elif ord(word[i]) + shift > 90:
+        elif ord(word[i]) + shift > 91:
             new_word += chr(ord(word[i]) + shift - 26)
         else:
             new_word += chr(ord(word[i]) + shift + 26)
@@ -39,15 +45,20 @@ def shiftWord(word, shift):
 
 
 def fileInp():
+    """Takes input text from a user supplied .txt file, returns text from file
+    """
     text = ""
     while True:
         filename = input("Enter the path to the .txt file you wish to use, or "
                          "c to cancel:\n")
         if filename not in ["c", "C"]:
             while filename[-4:] != ".txt":
-                filename = input("File must end in .txt, enter the path to a"
-                                 ".txt file, or c to cancel:\n")
-                continue
+                filename = input("File must end in .txt\nPlease enter the path"
+                                 "to a .txt file, or c to cancel:\n")
+                if filename in ["c", "C"]:
+                    return
+                else:
+                    continue
             try:
                 f = open(filename, "r")
             except FileNotFoundError:
@@ -62,34 +73,31 @@ def fileInp():
 
 
 def askFile():
+    """Asks user if they want to manually or from a file, then takes the input
+    or passes control to fileInp()"""
     while True:
         choice = input(
             "Would you like to enter the text manually(1) or from a .txt "
             "file(2)?\n")
         if choice == "1":
             text = input("\nEnter your text:\n")
-            break
         elif choice == "2":
             text = fileInp()
-            if text is None or text == "":
-                continue
-            else:
-                break
-        else:
+        if text is None or text == "":
+            print("Please enter some text or provide a non-empty file.\n")
             continue
+        else:
+            break
     return text
 
 
 def encrypt():
-    while True:
-        text = askFile()
-        if text is None or text == "":
-            continue
-        else:
-            break
+    """Encrypts the text with a caesar cipher of user input shift value"""
+    text = askFile()
     shift = getShift()
     enc_text = shiftText(text, shift)
     print("\nHere is your encrypted text:\n\n" + enc_text)
+    # Allows user to save the text to a file locally called caesarEncrypted.txt
     while True:
         inp = input("\nWould you like to save to file? (y/n)\n")
         if inp not in ["y", "Y", "n", "N"]:
@@ -105,12 +113,9 @@ def encrypt():
 
 
 def decrypt():
-    while True:
-        text = askFile()
-        if text is None:
-            continue
-        else:
-            break
+    """Decrypts input text with a given cipher value or passes to guessShift
+    to attempt to guess the shift value"""
+    text = askFile()
     while True:
         choice = input("Do you know the shift value?(y/n)\n")
         if choice in ["Y", "y"]:
@@ -124,6 +129,7 @@ def decrypt():
             continue
     dec_text = shiftText(text, -shift)
     print("\nHere is your decrypted text:\n\n" + dec_text)
+    # Allows user to save text in a file locally named caesarDecrypted.txt
     while True:
         inp = input("\nWould you like to save to file? (y/n)\n")
         if inp not in ["y", "Y", "n", "N"]:
@@ -139,6 +145,7 @@ def decrypt():
 
 
 def checkGoodness(text, shift):
+    """Returns sum of letter frequency values from each word in the text"""
     total_goodness = 0.0
     old_text = text.split()
     for word in old_text:
@@ -147,6 +154,8 @@ def checkGoodness(text, shift):
 
 
 def wordGoodness(word, shift):
+    """Evaluates the goodness value of a word from the letter frequency value
+    in English"""
     letter_goodness = [
         0.0817, 0.0149, 0.0278, 0.0425, 0.127, 0.0223, 0.0202, 0.0609, 0.0697,
         0.0015, 0.0077, 0.0402, 0.0241, 0.0675, 0.0751, 0.0193, 0.0009, 0.0599,
@@ -162,8 +171,11 @@ def wordGoodness(word, shift):
 
 
 def guessShift(text):
+    """Compares the goodness values of the text for each possible shift value
+    and returns the shift value with the highest likelihood according to
+    letter frequency"""
     goodness_values = []
-    for i in range(27):
+    for i in range(26):
         goodness_values.append(checkGoodness(text, i))
     best_guess = goodness_values.index(max(goodness_values))
     return best_guess
